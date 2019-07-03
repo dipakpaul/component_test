8e5472e4e0b02bc7b18219e0e3f298fdb68b07ad
DIPAK PAUL <dipakpaul@DIPAK-PAULs-MacBook-Pro.local>
2019-07-03 10:08:08 +0530

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NamesComponent } from './names/names.component';
+import { NameComponent } from './name/name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NamesComponent,
+    NameComponent
   ],
   imports: [
     BrowserModule,
